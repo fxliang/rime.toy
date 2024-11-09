253979c9b669fc2ef83d8988fef703fb2b827723
fxliang <fx.liang@outlook.com>
2024-11-09 23:12:02 +0800

    ui: PROCESS_PER_MONITOR_DPI_AWARE, and set antialias

--- WeaselUI/WeaselPanel.cpp
+++ WeaselUI/WeaselPanel.cpp
@@ -426,6 +426,8 @@ void D2D::InitDirect2D() {
   // and exposes drawing commands
   HR(d2Device->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE,
                                    dc.ReleaseAndGetAddressOf()));
+  dc->SetAntialiasMode(D2D1_ANTIALIAS_MODE_PER_PRIMITIVE);
+  dc->SetTextAntialiasMode(D2D1_TEXT_ANTIALIAS_MODE_CLEARTYPE);
   // Retrieve the swap chain's back buffer
   HR(swapChain->GetBuffer(
       0, // index
