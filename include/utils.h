c561adb70333d6795308978bd6880d25e1f924f9
fxliang <fx.liang@outlook.com>
2024-11-11 12:01:00 +0800

    fix: string convertion with unique_ptr

--- include/utils.h
+++ include/utils.h
@@ -1,45 +1,45 @@
 ﻿#pragma once
 
+#include <memory>
 #include <sstream>
 #include <string>
 #include <windows.h>
 
+// convert string to wstring, in code_page
 inline std::wstring string_to_wstring(const std::string &str,
                                       int code_page = CP_ACP) {
   // support CP_ACP and CP_UTF8 only
-  if (code_page != 0 && code_page != CP_UTF8)
+  if (code_page != CP_ACP && code_page != CP_UTF8)
     return L"";
-  // calc len
-  int len =
-      MultiByteToWideChar(code_page, 0, str.c_str(), (int)str.size(), NULL, 0);
+  int len = MultiByteToWideChar(code_page, 0, str.c_str(), (int)str.size(),
+                                nullptr, 0);
   if (len <= 0)
     return L"";
-  std::wstring res;
-  WCHAR *buffer = new WCHAR[len + 1];
-  MultiByteToWideChar(code_page, 0, str.c_str(), (int)str.size(), buffer, len);
-  buffer[len] = '\0';
-  res.append(buffer);
-  delete[] buffer;
-  return res;
+  // Use unique_ptr to manage the buffer memory automatically
+  std::unique_ptr<WCHAR[]> buffer = std::make_unique<WCHAR[]>(len + 1);
+  MultiByteToWideChar(code_page, 0, str.c_str(), (int)str.size(), buffer.get(),
+                      len);
+  buffer[len] = L'\0'; // Null-terminate the wide string
+  return std::wstring(buffer.get());
 }
+// convert wstring to string, in code_page
 inline std::string wstring_to_string(const std::wstring &wstr,
                                      int code_page = CP_ACP) {
   // support CP_ACP and CP_UTF8 only
-  if (code_page != 0 && code_page != CP_UTF8)
+  if (code_page != CP_ACP && code_page != CP_UTF8)
     return "";
   int len = WideCharToMultiByte(code_page, 0, wstr.c_str(), (int)wstr.size(),
-                                NULL, 0, NULL, NULL);
+                                nullptr, 0, nullptr, nullptr);
   if (len <= 0)
     return "";
-  std::string res;
-  char *buffer = new char[len + 1];
-  WideCharToMultiByte(code_page, 0, wstr.c_str(), (int)wstr.size(), buffer, len,
-                      NULL, NULL);
-  buffer[len] = '\0';
-  res.append(buffer);
-  delete[] buffer;
-  return res;
+  // Use unique_ptr to manage the buffer memory automatically
+  std::unique_ptr<char[]> buffer = std::make_unique<char[]>(len + 1);
+  WideCharToMultiByte(code_page, 0, wstr.c_str(), (int)wstr.size(),
+                      buffer.get(), len, nullptr, nullptr);
+  buffer[len] = '\0'; // Null-terminate the string
+  return std::string(buffer.get());
 }
+
 #define wtou8(x) wstring_to_string(x, CP_UTF8)
 #define wtoacp(x) wstring_to_string(x)
 #define u8tow(x) string_to_wstring(x, CP_UTF8)
