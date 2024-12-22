9f99769b9db3d5b17f52d0699d4a0d0d35b26349
fxliang <fx.liang@outlook.com>
2024-12-22 21:30:00 +0800

    build: update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,5 @@
 local project_name = "rime.toy"
 
-
 add_includedirs("./include")
 add_defines("UNICODE", "_UNICODE", "_WIN32_WINNT=0x0603", "TOY_FEATURE")
 includes("WeaselUI")
@@ -28,27 +27,25 @@ target(project_name)
     add_ldflags("-municode -mwindows", {force = true})
   end
 
-  if is_arch("x64") or is_arch("x86_64") then
-    add_linkdirs("lib64")
-  elseif is_arch("x86") then
-    add_linkdirs("lib")
-  end
+  add_linkdirs(is_arch("x86") and "lib" or "lib64")
+
   -- copy build output to $(projectdir)
   after_build(function(target)
-    os.cp(path.join(target:targetdir(), project_name .. ".exe"), "$(projectdir)")
+    local prjoutput = path.join(target:targetdir(), target:filename())
+    print("copy " .. prjoutput .. " to $(projectdir)")
+    os.trycp(prjoutput, "$(projectdir)")
     if is_mode("debug") and is_plat('windows') then
-      os.cp(path.join(target:targetdir(), project_name .. ".pdb"), "$(projectdir)")
+      os.trycp(path.join(target:targetdir(), target:name() .. ".pdb"), "$(projectdir)")
     end
-    if is_arch("x86") then
-      os.cp("$(projectdir)/lib/rime.dll", "$(projectdir)")
-    else
-      os.cp("$(projectdir)/lib64/rime.dll", "$(projectdir)")
-    end
+    local rimelib=(is_arch('x86') and path.join("$(projectdir)", "lib/rime.dll")
+      or path.join("$(projectdir)", "lib64/rime.dll"))
+    print("copy " .. rimelib .. " to $(projectdir)")
+    os.trycp(rimelib, "$(projectdir)")
   end)
 
   local version_major = "0"
   local version_minor = "0"
-  local version_patch = "4"
+  local version_patch = "5"
   local version = "\"" .. version_major .. "." .. version_minor .. "." .. version_patch .. "\""
   add_defines("VERSION_INFO=="..version)
   -- generate src/rime.toy.rc before build if needed
@@ -56,11 +53,24 @@ target(project_name)
     import("core.base.text")
     local rc_template = path.join(os.projectdir(), "src/rime.toy.rc.in")
     local rc_output = path.join(os.projectdir(), "src/rime.toy.rc")
-    local content = io.readfile(rc_template)
-    content = content:gsub("${VERSION_MAJOR}", version_major)
-    content = content:gsub("${VERSION_MINOR}", version_minor)
-    content = content:gsub("${VERSION_PATCH}", version_patch)
-    local commit_id = os.iorun("git rev-parse --short HEAD"):gsub("\n", "")
-    content = content:gsub("${TAG_SUFFIX}", commit_id)
-    io.writefile(rc_output, content)
+    local function generate_rc()
+      local content = io.readfile(rc_template)
+      content = content:gsub("${VERSION_MAJOR}", version_major)
+      content = content:gsub("${VERSION_MINOR}", version_minor)
+      content = content:gsub("${VERSION_PATCH}", version_patch)
+      local commit_id = os.iorun("git rev-parse --short HEAD"):gsub("\n", "")
+      content = content:gsub("${TAG_SUFFIX}", commit_id)
+      io.writefile(rc_output, content)
+    end
+    local function check_version()
+      local rc = io.readfile(rc_output)
+      local commit_id = os.iorun("git rev-parse --short HEAD"):gsub("\n", "")
+      local version_str = version_major..'.'..version_minor..'.'..version_patch..'.'..commit_id
+      return string.find(rc, version_str)
+    end
+    -- no rc file, or version info not match
+    if not os.isfile(rc_output) or not check_version() then
+      print("generate new rc file: " .. rc_output)
+      generate_rc()
+    end
   end)
