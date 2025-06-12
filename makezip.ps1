56f3161bed765847a43513a8e8982601d6e7dc1b
fxliang <fx.liang@outlook.com>
2025-06-12 20:56:42 +0800

    ci: separate pdbs zip

--- makezip.ps1
+++ makezip.ps1
@@ -1,6 +1,7 @@
 param([string]$zipFileName = "rime.toy.zip")
 # 定义要打包的文件和目录
-$itemsToArchive = @("shared", "rime.toy.exe", "rime.toy.pdb", "rime.dll", "rime.pdb", "rime.toy.json", "restart_rime.toy.bat", "LICENSE.txt", "README.md")
+$itemsToArchive = @("shared", "rime.toy.exe", "rime.dll", "rime.toy.json", "restart_rime.toy.bat", "LICENSE.txt", "README.md")
+$pdbFiles = @("rime.toy.pdb", "rime.pdb")
 # 删除已存在的压缩包（如果存在）
 if (Test-Path $zipFileName) {
   Remove-Item $zipFileName
@@ -8,9 +9,17 @@ if (Test-Path $zipFileName) {
 # 打包文件和目录
 foreach ($item in $itemsToArchive) {
   if (Test-Path $item) {
-    Compress-Archive -Path $item -Update -DestinationPath $zipFileName
+    Compress-Archive -Path $item -Update -DestinationPath $zipFileName -Update
   } else {
     Write-Host "Warning: $item does not exist and will not be included in the archive."
+  }
+}
+$pdbZipFileName = [System.IO.Path]::ChangeExtension($zipFileName, ".pdbs.zip")
+foreach ($pdbFile in $pdbFiles) {
+  if (Test-Path $pdbFile) {
+    Compress-Archive -Path $pdbFile -Update -DestinationPath $pdbZipFileName -Update
+  } else {
+    Write-Host "Warning: $pdbFile does not exist and will not be included in the PDB archive."
   }
 }
 Write-Host "Files and directories have been archived into $zipFileName"
